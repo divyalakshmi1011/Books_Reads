985d43397653891491506bbf2c52c11fcd92397d
Moksh <mokshdipikasikkem@msitprogram.net>
2020-04-26 08:23:45 +0530

    added review page

--- Project1/models.py
+++ Project1/models.py
@@ -3,8 +3,7 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model) :
-
+class User(db.Model):
     __tablename__="UserDetails"
 
     name = db.Column(db.String, nullable=False)
@@ -40,3 +39,17 @@ class Book(db.Model) :
     def __repr__(self) :
 
         return "ISBN : " + self.isbn + " | Title : " + self.title + " | Author : " + self.author + " | Year : " + self.year
+
+class Review(db.Model):
+    _tablename_ = "Review"
+    email = db.Column(db.String, primary_key=True)
+    isbn = db.Column(db.String, nullable=False, primary_key=True)
+    rating = db.Column(db.String, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    
+    def __init__(self, username, isbn, rating, review) :
+        
+        self.isbn = isbn
+        self.username = username
+        self.rating = rating
+        self.review = review
